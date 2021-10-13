e9dfed3302ef3f847635f5d813b9e6c971405ddd
mohamed <ragabmohame6@gmail.com>
2021-10-13 16:54:09 +0200

    Create home page

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="home in homes" :key="home.objectID" cols="3">
+        <home-card :home="home" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import homes from '@/data/homes'
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    }
+  },
+}
+</script>
