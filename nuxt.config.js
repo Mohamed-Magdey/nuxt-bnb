38dc0ddc27b6073d3b59115138d0deb684017ee9
mohamed <ragabmohame6@gmail.com>
2021-10-14 13:30:29 +0200

    Add map

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'nuxt-leaflet',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
