e6cfb0e4d68b58c0461245d9293c45a5f9d6ab9f
mohamed <ragabmohame6@gmail.com>
2021-10-15 14:59:38 +0200

    Setup api & create getHome api

---
+++ services/Api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiCall = axios.create({
+  baseURL: process.env.baseUrl,
+  // withCredentials: true, => makes cors error
+  headers: {
+    'X-Algolia-API-Key': process.env.apiKey,
+    'X-Algolia-Application-Id': process.env.appId,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000
+})
+
+export default {
+  getHome(homeId) {
+    return apiCall.get(`/homes/${homeId}`)
+  }
+}
