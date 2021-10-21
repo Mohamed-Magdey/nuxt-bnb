63443f2e75fb43d2c6fc8f50a34bb48d8bf347d3
mohamed <ragabmohame6@gmail.com>
2021-10-21 15:40:53 +0200

    Create Paragraph component

---
+++ components/Paragraph.vue
@@ -0,0 +1,26 @@
+<template>
+  <p :style="`color: ${paragarphColor}`">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    paragarphColor() {
+      return this.$vuetify.theme.isDark ? 'rgba(255,255,255,0.9)' : '#494e61'
+    },
+  },
+}
+</script>
+
+<style scoped>
+h2 + p {
+  font-size: 1.125rem;
+  margin: 0;
+}
+</style>
