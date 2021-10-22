c659bb51fd1ed2ee15b55d15d4ed013f0379602e
mohamed <ragabmohame6@gmail.com>
2021-10-22 19:47:50 +0200

    Refactor header using auth

--- components/Header.vue
+++ components/Header.vue
@@ -7,12 +7,14 @@
     <v-btn to="/" text>Home</v-btn>
     <v-btn text @click="toggleTheme">{{ toggleDark }}</v-btn>
     <div class="d-flex align-center mx-2">
-      <v-img src="/images/icons/house.svg"></v-img>
-      <div class="name">Host</div>
-      <!-- <v-avatar>
-        <img src="/images/user.jpg" alt="user" />
-      </v-avatar> -->
-      <div id="googleButton"></div>
+      <template v-if="isLoggedIn">
+        <v-img src="/images/icons/house.svg"></v-img>
+        <div class="name">{{ user.fullName }}</div>
+        <v-avatar>
+          <img :src="user.profileUrl" :alt="user.fullName" />
+        </v-avatar>
+      </template>
+      <div v-show="!isLoggedIn" id="googleButton"></div>
     </div>
   </v-app-bar>
 </template>
@@ -22,6 +24,12 @@ export default {
   computed: {
     toggleDark() {
       return this.$vuetify.theme.isDark ? 'Light' : 'Dark'
+    },
+    user() {
+      return this.$store.state.auth.user
+    },
+    isLoggedIn() {
+      return this.$store.state.auth.isLoggedIn
     },
   },
   methods: {
