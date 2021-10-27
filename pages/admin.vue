28881d589adb11bbf4e9c731d6eadc579f1fff7c
mohamed <ragabmohame6@gmail.com>
2021-10-27 11:11:40 +0200

    Protecting the  admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -6,5 +6,14 @@
 </template>
 
 <script>
-export default {}
+import Cookie from 'js-cookie'
+
+export default {
+  fetch() {
+    const context = this.$nuxt.context
+    if (!Cookie.get(context.env.auth.cookieName)) {
+      context.redirect('/no-access/')
+    }
+  },
+}
 </script>
