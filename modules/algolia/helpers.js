208f930c34ba2736c8f40496cfff552a7f87d236
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-10-29 13:37:26 +0200

    Update helpers

--- modules/algolia/helpers.js
+++ modules/algolia/helpers.js
@@ -17,3 +17,12 @@ export function sendJSON(data, res) {
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify(data))
 }
+
+export function rejectHitBadRequest(res) {
+  res.statusCode = 400
+  res.end()
+}
+
+export function hasBadBody(req) {
+  return !req.body || Object.keys(req.body).length === 0
+}
