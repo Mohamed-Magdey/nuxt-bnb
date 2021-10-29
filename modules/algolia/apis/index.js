f40350ef5895a04174ff60e837f304d280440a18
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-10-29 13:37:10 +0200

    Add user api to index

--- modules/algolia/apis/index.js
+++ modules/algolia/apis/index.js
@@ -1,10 +1,12 @@
 import { getAxiosInstance } from '../helpers'
 import userApi from './user'
+import homesApi from './homes'
 
 export default (app, algoliaConfig) => {
   const apiCall = getAxiosInstance(app, algoliaConfig)
 
   return {
-    user: userApi(apiCall)
+    user: userApi(apiCall),
+    homes: homesApi(apiCall)
   }
 }
