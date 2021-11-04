5544eead5d1520ce93e3eda21c8cb07801f349ee
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-04 14:00:39 +0200

    Update home api to use availability

--- modules/algolia/apis/homes.js
+++ modules/algolia/apis/homes.js
@@ -9,6 +9,17 @@ export default (apiCall) => {
     },
     create: async (homeId, payload) => {
       try {
+        const availability = []
+        payload.availabilityRanges.forEach(range => {
+          const start = new Date(range.start).getTime() / 1000
+          const end = new Date(range.end).getTime() / 1000
+          for (let day = start; day <= end; day += 86400) {
+            availability.push(day)
+          }
+        })
+
+        delete payload.availabilityRanges
+        payload.availability = availability
         return await apiCall.put(`/homes/${homeId}`, payload)
       } catch (err) {
         return err
