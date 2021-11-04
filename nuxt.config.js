61f8a2c48ba06714e5a09cec53b6bdee611675dc
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-04 13:23:34 +0200

    Create vCalendar plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/base.js',
-    '@plugins/auth.client.js'
+    '@/plugins/auth.client.js',
+    '@/plugins/vCalendar.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
