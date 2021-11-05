c402245f5ebe1ca34bc0b46e3dc10cdd084e1e5a
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-05 16:47:15 +0200

    Fix head with fetch issue

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -21,7 +21,7 @@ export default {
   name: 'Property',
   data() {
     return {
-      home: {},
+      home: null,
       reviews: [],
       user: {},
     }
@@ -43,28 +43,30 @@ export default {
   },
   head() {
     return {
-      title: this.home.title,
+      title: this.home?.title,
       meta: [
         { hid: 'og-type', property: 'og:type', content: 'website' },
-        { hid: 'og-title', property: 'og:title', content: this.home.title },
+        { hid: 'og-title', property: 'og:title', content: this.home?.title },
         {
           hid: 'og-desc',
           property: 'og:description',
-          content: this.home.description,
+          content: this.home?.description,
         },
         {
           hid: 'og-image',
           property: 'og:image',
-          content: this.$img(
-            this.home.images[0],
-            { width: 1200 },
-            { provider: 'cloudinary' }
-          ),
+          content:
+            this.home &&
+            this.$img(
+              this.home.images[0],
+              { width: 1200 },
+              { provider: 'cloudinary' }
+            ),
         },
         {
           hid: 'og-url',
           property: 'og:url',
-          content: `${process.env.rootUrl}/home/${this.home.objectID}`,
+          content: `${process.env.rootUrl}/home/${this.home?.objectID}`,
         },
         { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
       ],
