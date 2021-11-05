fc9e2c3d6d9214534d56a9dcf03a99fb39e6bf8b
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-05 14:24:23 +0200

    Configure production url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  rootUrl: process.env.NODE_ENV === 'production' ? 'https://nuxt-bnb-two.vercel.app' : 'http://localhost:3000',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: 'Mastering Nuxt: %s',
