965360b4d3209269e6d1b0fd1887c52f56c67cd5
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-05 15:48:14 +0200

    Create stirpe web hooks

--- modules/stripe/index.js
+++ modules/stripe/index.js
@@ -14,6 +14,14 @@ export default function () {
     app.use('/api/stripe/create-session', createSession)
   })
 
+  this.nuxt.hook('render:setupMiddleware', (app) => {
+    app.use('/hooks/stripe', (req, res) => {
+      const meta = req.body.data.object.metadata
+
+      res.end(`${meta.identityId} booked ${meta.homeId}!!!!!`)
+    })
+  })
+
   async function createSession(req, res) {
     const body = req.body
     if (!body || !body.homeId || !body.start || !body.end || !body.start >= body.end) {
@@ -23,6 +31,12 @@ export default function () {
     const home = (await apis.homes.get(body.homeId)).data
     const nights = (body.end - body.start) / 86400
     const session = await stripe.checkout.sessions.create({
+      metadata: {
+        identityId: req.identity.id,
+        homeId: body.homeId,
+        start: body.start,
+        end: body.end
+      },
       payment_method_types: ['card'],
       mode: 'payment',
       success_url: `${rootUrl}/home/${body.homeId}/?result=success`,
