2e7f68e529e2c0f3dfecd9d59a0de0dc3678ccd7
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-08 12:34:16 +0200

    Create gallery section & add styles

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,31 @@
     Fetching mountains...
   </v-container>
   <error v-else-if="$fetchState.error" :error="$fetchState.error" />
-  <v-container v-else fluid>
-    <v-row>
-      <v-col v-for="home in homes" :key="home.objectID" cols="3">
-        <nuxt-link :to="`/home/${home.objectID}`" prefetch>
-          <home-card :home="home" />
-        </nuxt-link>
+  <v-container v-else>
+    <v-row tag="section" class="my-5">
+      <v-col>
+        <div class="gallery rounded-xl d-flex align-end justify-center">
+          <h2 class="mx-6">
+            - Settle in somewhere new. Discover stays to live, work, or just
+            relax.
+          </h2>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row
+      tag="section"
+      :style="`backgroundColor: ${CardsColor}`"
+      class="py-20"
+    >
+      <v-col>
+        <h2 class="text-color--text">Recently Added</h2>
+        <v-row class="justify-space-between">
+          <v-col v-for="home in homes" :key="home.objectID" cols="12" sm="3">
+            <nuxt-link :to="`/home/${home.objectID}`" prefetch>
+              <home-card :home="home" />
+            </nuxt-link>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -44,5 +63,31 @@ export default {
       ],
     }
   },
+  computed: {
+    CardsColor() {
+      return this.$vuetify.theme.isDark ? '#272727;' : '#f6f8fb'
+    },
+  },
 }
 </script>
+
+<style scoped>
+.gallery {
+  background-image: url('/images/beach.jpg');
+  background-size: cover;
+  height: 37rem;
+}
+
+h2 {
+  font-weight: 700 !important;
+  font-size: 1.875rem;
+}
+
+.gallery h2 {
+  color: #fff;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
